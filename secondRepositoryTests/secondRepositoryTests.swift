883d4735b4232234517955f183eb2fa84fab2a68
Edward McGuiggan <emcguiggan@local-01-lt-044.local>
2018-01-30 11:01:57 -0500

    Initial Commit

---
+++ secondRepositoryTests/secondRepositoryTests.swift
@@ -0,0 +1,36 @@
+//
+//  secondRepositoryTests.swift
+//  secondRepositoryTests
+//
+//  Created by Edward McGuiggan on 1/30/18.
+//  Copyright © 2018 Localytics. All rights reserved.
+//
+
+import XCTest
+@testable import secondRepository
+
+class secondRepositoryTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
